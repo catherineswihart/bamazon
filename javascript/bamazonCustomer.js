6d8ab63cedcb32a6f19c6a46af1c844bdc66e25d
Catherine Swihart <catherineswihart@gmail.com>
2019-10-16 21:06:15 -0500

    sql

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var chalk = require('chalk');
-var Table = require('cli-table');
+// var chalk = require('chalk');
+// var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: 'localhost',
